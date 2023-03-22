b2531f7d8c6362057f7843961778636b042c4e4a
Ruslan Akimov <rasselkom@gmail.com>
2023-03-22 14:29:46 +0300

    first commit

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,6 +1,7 @@
 import './components/slider';
-import './components/popup';
-import './components/tabs';
-import './components/burger';
 import './components/acordion';
+import './components/tabs';
+import './components/popup';
+import './components/burger';
+
 
